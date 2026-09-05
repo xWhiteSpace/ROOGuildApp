a458858faf93ec3e6a4b920fe44616da1c588bfb
WhiteSpace <84615169+xWhiteSpace@users.noreply.github.com>
2026-09-05 16:43:57 +0900

    added diags

--- frontend/api/discord-token-exchange.js
+++ frontend/api/discord-token-exchange.js
@@ -91,14 +91,18 @@ export default async function handler(req, res) {
     }
 
     let member = null;
+    let memberStatus = guildId ? 'not_fetched' : 'no_guild_id';
     if (guildId) {
       const memberResponse = await fetch(`https://discord.com/api/users/@me/guilds/${guildId}/member`, {
         headers: { Authorization: `Bearer ${tokenPayload.access_token}` },
       });
+      memberStatus = memberResponse.status;
       if (memberResponse.ok) {
         const rawMember = await memberResponse.json().catch(() => null);
         if (rawMember && Array.isArray(rawMember.roles)) {
           member = { nick: rawMember.nick || null, roles: rawMember.roles };
+        } else {
+          memberStatus = `ok_but_no_roles`;
         }
       }
     }
@@ -112,6 +116,7 @@ export default async function handler(req, res) {
         global_name: user.global_name,
       },
       member,
+      memberStatus,
     });
   } catch (err) {
     console.error('[oauth-bridge] exception', err);
